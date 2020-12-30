d505116e9a17ff6532fa8d88b7ae5dc30922756c
Borislav Stoyanov <bostoycontact@gmail.com>
2020-12-30 18:39:45 +0200

    html paths fixed + static files

--- handlers/static-files.js
+++ handlers/static-files.js
@@ -1,27 +1,26 @@
 const url = require('url');
 const fs = require('fs');
-const path = require('path');
 
 function getContentType(url) {
     if (url.endsWith('css')) {
         return 'text/css';
-    } else if (url.endsWith('html')) {
-        return 'text/html';
-    } else if (url.endsWith('png')) {
-        return 'image/png';
-    } else if (url.endsWith('txt')) {
-        return 'text/plain';
     } else if (url.endsWith('ico')) {
         return 'image/x-icon';
-    }
+    } else if (url.endsWith('jpg')) {
+        return 'image/jpeg';
+    } else if (url.endsWith('jpeg')) {
+        return 'image/jpeg';
+    } else if (url.endsWith('png')) {
+        return 'image/png';
+    } else
+        return true;
 }
 
-module.exports = ((req, res) => {
+module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     if (pathname.startsWith('/content') && req.method === 'GET') {
-        if (pathname.endsWith('html') || pathname.endsWith('png') || pathname.endsWith('txt') || pathname.endsWith('ico')) {
-
+        if (pathname.endsWith('ico') || pathname.endsWith('jpeg') || pathname.endsWith('jpg') || pathname.endsWith('png')) {
             fs.readFile(`./${pathname}`, (err, data) => {
                 if (err) {
                     console.log(err);
@@ -29,15 +28,30 @@ module.exports = ((req, res) => {
                     res.write('404 File Not Found');
                     res.end();
                     return;
-                };
-                res.writeHead(200, { 'Content-Type': `${getContentType(pathname)}` });
+                }
+
+                console.log(pathname)
+                res.writeHead(200, { 'Content-Type': getContentType(pathname) });
                 res.write(data);
                 res.end();
+            });
+        } else {
+            fs.readFile(`./${pathname}`, 'utf-8', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.write('404 File Not Found');
+                    res.end();
+                    return;
+                }
 
-            })
+                console.log(pathname)
+                res.writeHead(200, { 'Content-Type': getContentType(pathname) });
+                res.write(data);
+                res.end();
+            });
         }
-
+    } else {
+        return true;
     }
-
-
-});
+};
