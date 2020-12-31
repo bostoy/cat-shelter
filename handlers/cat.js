2ba0c45cec516e617505719638e2c48dad93a22b
Borislav Stoyanov <bostoycontact@gmail.com>
2020-12-31 12:24:52 +0200

    add-cat, add-breed requests not working

--- handlers/cat.js
+++ handlers/cat.js
@@ -5,6 +5,7 @@ const qs = require('querystring');
 const formidable = require('formidable');
 const breeds = require('../data/breeds');
 const cats = require('../data/cats.json');
+const globalPath = __dirname.toString().replace('handlers', '');
 
 module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
@@ -34,7 +35,7 @@ module.exports = (req, res) => {
 
     } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
 
-        const filePath = path.normalize(path.join(__dirname, './views/addBreed.html'));
+        const filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
 
         fs.readFile(filePath, (err, data) => {
             if (err) {
@@ -50,5 +51,66 @@ module.exports = (req, res) => {
         });
 
 
+    } else if (pathname === '/cats/add-cat' && req.method('POST')) {
+        let form = new formidable.IncomingForm();
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                throw (err);
+            }
+            const oldPath = files.upload.path;
+            const newPath = path.normalize(path.join(globalPath, '/content/images' + files.upload.name));
+
+            fs.rename(oldPath, newPath, (err) => {
+                if (err) {
+                    throw (err);
+                }
+                console.log('Image successfully uploaded');
+            });
+
+            fs.readFile('./data/cats.json', (err, data) => {
+                if (err) {
+                    throw (err);
+                }
+
+                const allCats = JSON.parse(data);
+                allCats.push({ id: (cats.length + 1).toString(), ...fields, image: files.upload.name });
+                const json = JSON.stringify(allCats);
+            });
+
+            fs.writeFile('./data/cats.json', json, (err) => {
+                if (err) {
+                    throw (err);
+                }
+                console.log("Cat successfully added");
+            });
+
+            res.writeHead(301, { 'location': '/' });
+            res.end();
+        })
+    } else if (pathname === '/cats/add-breed' && req.method('POST')) {
+
+        let formData = '';
+        req.on('data', data => {
+            formData += data;
+        });
+
+        req.on('end', () => {
+            const body = qs.parse(formData);
+
+            fs.readFile('./data/breeds.json', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                let breeds = JSON.parse(data);
+                breeds.push(body.breed);
+                const json = JSON.stringify(breeds);
+                fs.writeFile('./data/breeds.json', json, () => {
+                    console.log(`${body.breed} was added successfully to the breeds.`)
+                })
+            })
+        });
+
+        res.writeHead(301, { 'location': '/' });
+        res.end();
     }
 }
