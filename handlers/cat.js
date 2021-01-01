188b97a2954ffa3a809c0df965a87958ece2072d
Borislav <bostoycontact@gmail.com>
2021-01-01 14:54:20 +0200

    create breed functionality + db

--- handlers/cat.js
+++ handlers/cat.js
@@ -5,6 +5,7 @@ const qs = require('querystring');
 const formidable = require('formidable');
 const breeds = require('../data/breeds');
 const cats = require('../data/cats.json');
+const { request } = require('http');
 const globalPath = __dirname.toString().replace('handlers', '');
 
 module.exports = (req, res) => {
@@ -51,48 +52,16 @@ module.exports = (req, res) => {
         });
 
 
-    } else if (pathname === '/cats/add-cat' && req.method('POST')) {
-        let form = new formidable.IncomingForm();
-        form.parse(req, (err, fields, files) => {
-            if (err) {
-                throw (err);
-            }
-            const oldPath = files.upload.path;
-            const newPath = path.normalize(path.join(globalPath, '/content/images' + files.upload.name));
+    } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
 
-            fs.rename(oldPath, newPath, (err) => {
-                if (err) {
-                    throw (err);
-                }
-                console.log('Image successfully uploaded');
-            });
-
-            fs.readFile('./data/cats.json', (err, data) => {
-                if (err) {
-                    throw (err);
-                }
-
-                const allCats = JSON.parse(data);
-                allCats.push({ id: (cats.length + 1).toString(), ...fields, image: files.upload.name });
-                const json = JSON.stringify(allCats);
-            });
-
-            fs.writeFile('./data/cats.json', json, (err) => {
-                if (err) {
-                    throw (err);
-                }
-                console.log("Cat successfully added");
-            });
-
-            res.writeHead(301, { 'location': '/' });
-            res.end();
-        })
-    } else if (pathname === '/cats/add-breed' && req.method('POST')) {
+    } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
 
         let formData = '';
         req.on('data', data => {
             formData += data;
         });
+
+        console.log(formData);
 
         req.on('end', () => {
             const body = qs.parse(formData);
@@ -104,12 +73,17 @@ module.exports = (req, res) => {
                 let breeds = JSON.parse(data);
                 breeds.push(body.breed);
                 const json = JSON.stringify(breeds);
-                fs.writeFile('./data/breeds.json', json, () => {
-                    console.log(`${body.breed} was added successfully to the breeds.`)
+
+
+                fs.writeFile('./data/breeds.json', json, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    console.log(`${body.breed} was successfully added to the breeds`);
                 })
             })
         });
-
         res.writeHead(301, { 'location': '/' });
         res.end();
     }
